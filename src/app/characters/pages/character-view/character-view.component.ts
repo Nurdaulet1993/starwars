2a7bfa5afba42efc172cbb86125cf0137527093e
Nurdaulet <nurik93sat@gmail.com>
2022-09-04 16:27:43 +0600

    Character view details

--- src/app/characters/pages/character-view/character-view.component.ts
+++ src/app/characters/pages/character-view/character-view.component.ts
@@ -1,8 +1,16 @@
 import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
 import { PeopleApiService } from '@core/services/people-api.service';
 import { ActivatedRoute, Data } from '@angular/router';
-import { BehaviorSubject, map } from 'rxjs';
+import { BehaviorSubject, forkJoin, map, switchMap, takeWhile } from 'rxjs';
 import { Character } from '@app/characters/character.model';
+import { FilmsApiService } from '@core/services/films-api.service';
+import { Film } from '@app/films/film.model';
+import { IStarship } from '@app/starships/starship.model';
+import { StarshipsApiService } from '@core/services/starships-api.service';
+import { IVehicle } from '@app/vehicles/vehicle.model';
+import { VehiclesApiService } from '@core/services/vehicles-api.service';
+import { ISpecie } from '@app/species/specie.model';
+import { SpeciesApiService } from '@core/services/species-api.service';
 
 @Component({
   selector: 'app-character-view',
@@ -13,10 +21,18 @@ import { Character } from '@app/characters/character.model';
 export class CharacterViewComponent implements OnInit, OnDestroy {
   componentActive = true;
   character$ = new BehaviorSubject<Character>({ } as Character);
+  films$ = new BehaviorSubject<Film[]>([]);
+  starships$ = new BehaviorSubject<IStarship[]>([]);
+  vehicles$ = new BehaviorSubject<IVehicle[]>([]);
+  species$ = new BehaviorSubject<ISpecie[]>([])
 
   constructor(
     private peopleApiService: PeopleApiService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private filmsApiService: FilmsApiService,
+    private starshipsApiService: StarshipsApiService,
+    private vehiclesApiService: VehiclesApiService,
+    private speciesApiService: SpeciesApiService
   ) {}
 
   ngOnInit(): void {
@@ -25,6 +41,46 @@ export class CharacterViewComponent implements OnInit, OnDestroy {
         map((data: Data) => data['character'])
       )
       .subscribe(this.character$)
+
+    this.character$
+      .pipe(
+        takeWhile(() => this.componentActive),
+        map(character => character.filmIds),
+        switchMap(films => forkJoin(
+          ...films.map(filmId => this.filmsApiService.getFilm(filmId))
+        ))
+      )
+      .subscribe(this.films$);
+
+    this.character$
+      .pipe(
+        takeWhile(() => this.componentActive),
+        map(character => character.starshipIds),
+        switchMap(films => forkJoin(
+          ...films.map(filmId => this.starshipsApiService.getStarship(filmId))
+        ))
+      )
+      .subscribe(this.starships$);
+
+    this.character$
+      .pipe(
+        takeWhile(() => this.componentActive),
+        map(character => character.vehicleIds),
+        switchMap(films => forkJoin(
+          ...films.map(filmId => this.vehiclesApiService.getVehicle(filmId))
+        ))
+      )
+      .subscribe(this.vehicles$)
+
+    this.character$
+      .pipe(
+        takeWhile(() => this.componentActive),
+        map(character => character.starshipIds),
+        switchMap(films => forkJoin(
+          ...films.map(filmId => this.speciesApiService.getSpecie(filmId))
+        ))
+      )
+      .subscribe(this.species$)
 
   }
 
